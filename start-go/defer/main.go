df229b97a11bbd7c471973bbce3396bf872e9682
yooodleee <accia25@naver.com>
2025-06-18 22:23:44 +0900

    defer

---
+++ start-go/defer/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+func main() {
+	resp, err := http.Get("http://example.com")
+	
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	defer fmt.Println("Deferred cleanup")
+
+	fmt.Println("Response status:", resp.Status)
+}
