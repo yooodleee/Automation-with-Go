995ba8b6c5c1fe6361b77edbf6f05b5c0da18861
yooodleee <accia25@naver.com>
2025-06-19 18:52:06 +0900

    interfaces sample

---
+++ start-go/interfaces-sample/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+
+type CiscoIOS struct {
+	Hostname string
+	Platform string
+}
+
+func (r CiscoIOS) getUptime() int {
+	return 1
+}
+
+type CiscoNXOS struct {
+	Hostname string
+	Platform string
+	ACI bool
+}
+
+func (s CiscoNXOS) getUptime() int {
+	return 1
+}
+
+type NetworkDevice interface {
+	getUptime() int
+}
+
+func LastToReboot(r1, r2 NetworkDevice) bool {
+	return r1.getUptime() < r2.getUptime()
+}
+
+func main() {
+	ios := CiscoIOS{}
+	nexus := CiscoNXOS{}
+
+	if LastToReboot(ios, nexus) {
+		fmt.Println("IOS-XE has been running for less time, so it was the last to be roobted")
+		os.Exit(0)
+	}
+
+	fmt.Println("NXOS was the last one to reboot")
+}
